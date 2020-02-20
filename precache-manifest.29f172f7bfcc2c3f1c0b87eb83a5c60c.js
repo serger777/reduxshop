26adb8b71ef224a53273b06490a4a01ec4610404
sergey gerasimov <sergey.gerasimov@realize-it.ru>
2020-02-20 17:25:12 +0300

    Updates

---
+++ precache-manifest.29f172f7bfcc2c3f1c0b87eb83a5c60c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7585122bc8053a5e2f98fba197dcb057",
+    "url": "/reduxshop/index.html"
+  },
+  {
+    "revision": "c16b12539b19495bbb02",
+    "url": "/reduxshop/static/css/main.8a0ff0ae.chunk.css"
+  },
+  {
+    "revision": "9197050477f29e7105c4",
+    "url": "/reduxshop/static/js/2.20ef6daf.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/reduxshop/static/js/2.20ef6daf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c16b12539b19495bbb02",
+    "url": "/reduxshop/static/js/main.45e2e2eb.chunk.js"
+  },
+  {
+    "revision": "f43eb122ec8e1938b5bf",
+    "url": "/reduxshop/static/js/runtime-main.ae83019e.js"
+  }
+]);
